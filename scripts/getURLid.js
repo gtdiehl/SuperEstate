e428544ca66cb19c101c8bfdc61c2acef95470c2
Greg <greg.diehl.gtd@gmail.com>
2020-10-20 13:42:50 -0700

    Merging display and display_home pages

--- scripts/getURLid.js
+++ scripts/getURLid.js
@@ -1,2 +1,9 @@
 var url = document.URL;
 var id = url.substring(url.lastIndexOf('=') + 1);
+
+function getURLParam(parameter) {
+    var queryString = window.location.search;
+    var urlParams = new URLSearchParams(queryString);
+
+    return urlParams.get(parameter);
+}
