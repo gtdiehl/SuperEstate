db49f2711c5a659e5a37be7c27a53bc9a351ea6c
Greg <greg.diehl.gtd@gmail.com>
2020-10-21 23:52:01 -0700

    updating php

--- getPropertyPhotos.php
+++ getPropertyPhotos.php
@@ -3,11 +3,7 @@ $servername=getenv('DB_HOST');
 $username=getenv('DB_USERNAME');
 $password=getenv('DB_PASSWORD');
 $dbname=getenv('DB_DATABASE');
-$search_bedrooms=$_GET['bed_count'];
-$search_bathrooms=$_GET['bath_count'];
-$search_building=$_GET['building'];
-$search_minprice=$_GET['min_price'];
-$search_maxprice=$_GET['max_price'];
+$property_id=$_GET['id'];
 
 //create connection
 $conn=mysqli_connect($servername, $username, $password, $dbname);
@@ -16,49 +12,13 @@ $conn=mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn){
 	die("Connection failed to $servername: ".mysqli_connect_error());
 }
-$sql="SELECT * FROM `property` 
-	WHERE (`price` BETWEEN $search_minprice AND $search_maxprice) 
-	AND (`bed_count` >= $search_bedrooms)
-	AND (`bath_count` >= $search_bathrooms) 
-	AND (";
-
-
-$numItems = count($search_building);
-$i = 0;
-if ($numItems > 0) {
-    $sql .= "(";
-    foreach($search_building as &$type) {
-        $sql .= "`type` like '$type'";
-        if (++$i != $numItems) {
-            $sql .= " OR ";
-        }
-    }
-    $sql .= ")";
-}
-
-$sql .= ") ORDER BY `price` ASC";
+$sql="SELECT photo_count, video_count FROM `property` WHERE `property_id`=$property_id";
 
 $result=mysqli_query($conn,$sql);
 
-echo "<link rel='stylesheet' type='text/css' href='styles/styles.css'></link>";
-echo "<style type='text/css'>";
-echo "body{text-align:center; font-family:Microsoft YaHei;
-	font-weight:bold; background-color:#DEE7E8;}";
-echo "h1, p {text-align:left;}";
-echo "</style>";
-
-
 if (mysqli_num_rows($result)>0){
-	echo "<h1>".mysqli_num_rows($result)." results</h1>";
-    echo "<p>Sorted by Price (Low to High)</p>";
 	while ($row=mysqli_fetch_array($result)){
-        echo "<div class='property'>";
-        echo "<img src='images/house" . $row['property_id'] . ".jpg' alt='house'>";
-        echo "<div class='middle'><a href='display.html?id=".$row['property_id']."' target='_blank'>Details</a></div>";
-        echo "<div>$ " . number_format($row["price"]) . "</div></div>";
-        
+        echo "" . $row['photo_count'] . "," . $row['video_count'];
 	}
-}else{
-	echo "<p>No homes found.</p>";
 }
 ?>
