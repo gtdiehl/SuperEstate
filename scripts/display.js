06b67ea8b837a651e9d631285093e1ddb3befb6c
Greg <greg.diehl.gtd@gmail.com>
2020-10-22 00:46:27 -0700

    debugging

--- scripts/display.js
+++ scripts/display.js
@@ -12,6 +12,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("photo-container");
+  console.log("SL: " + slides);
 
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
