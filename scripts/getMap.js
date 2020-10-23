38a5569ab7730c47179ec3da27d6edf884dbdc1d
Greg <greg.diehl.gtd@gmail.com>
2020-10-22 19:42:33 -0700

    Adding Apple MapKit JS to display page

---
+++ scripts/getMap.js
@@ -0,0 +1,31 @@
+/* Token expires on Nov 05 2020 */
+const jwt_token = ***REMOVED***+ 
+                  ***REMOVED*** +
+                  ***REMOVED***
+
+function displayMap(address, mapElementName) {
+    var latitude, longitude, home;
+    var MarkerAnnotation = mapkit.MarkerAnnotation,
+        clickAnnotation;
+    var geocoder = new mapkit.Geocoder({
+        language: "en",
+        getsUserLocation: false
+    });
+
+    geocoder.lookup(address, function(err, data) {
+        latitude = data.results[0].coordinate.latitude;
+        longitude = data.results[0].coordinate.longitude;
+        home = new mapkit.Coordinate(latitude, longitude);
+        var homeAnnotation = new MarkerAnnotation(home, { color: "#f4a56d" });
+        map.showItems([homeAnnotation]);
+    });
+    
+    mapkit.init({
+        authorizationCallback: function(done) {
+            done(jwt_token);
+        },
+        language: "en"
+    });
+    var map = new mapkit.Map(mapElementName);
+    map.mapType = mapkit.Map.MapTypes.Hybrid;
+}
