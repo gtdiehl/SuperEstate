6184773b223db1963c426be1a19aa17a838f7228
Greg <greg.diehl.gtd@gmail.com>
2020-10-22 19:39:39 -0700

    Adding Apple mapkit js

---
+++ getPropertyAddress.php
@@ -0,0 +1,24 @@
+<?php
+$servername=getenv('DB_HOST');
+$username=getenv('DB_USERNAME');
+$password=getenv('DB_PASSWORD');
+$dbname=getenv('DB_DATABASE');
+$property_id=$_GET['id'];
+
+//create connection
+$conn=mysqli_connect($servername, $username, $password, $dbname);
+
+//check connection
+if (!$conn){
+	die("Connection failed to $servername: ".mysqli_connect_error());
+}
+$sql="SELECT address, city, zipcode FROM `property` WHERE `property_id`=$property_id";
+
+$result=mysqli_query($conn,$sql);
+
+if (mysqli_num_rows($result)>0){
+	while ($row=mysqli_fetch_array($result)){
+        echo "" . $row['address'] . ", " . $row['city'] . ", " . $row['zipcode'];
+	}
+}
+?>
