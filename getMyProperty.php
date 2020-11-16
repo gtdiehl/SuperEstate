3fbe4007ab5ef13f164915fbd120e74ba958e9f1
Greg <greg.diehl.gtd@gmail.com>
2020-11-16 12:38:03 -0800

    code

--- getMyProperty.php
+++ getMyProperty.php
@@ -18,11 +18,15 @@ $result=mysqli_query($conn,$sql);
 
 if (mysqli_num_rows($result)>0){
 	while ($row=mysqli_fetch_array($result)){
-        echo "" . $row['savedproperty'];
         $arr = $row['savedproperty'];
         $a = explode(',', $arr);
         foreach ($a as $b) {
-            echo $b;
+            echo "<div class='property'>";
+            echo "<img src='images/house/house" .$b. ".jpg' alt='house'>";
+            echo "<div class='middle'>";
+            echo "<a href='display.html?id=" .$b. " ' target='_blank'>Details</a></div>";
+            echo "<iframe src='price.php?id=" .$b. " ' width='100%' height='35'></iframe>";
+            echo "</div>";
         }
 	}
 }
