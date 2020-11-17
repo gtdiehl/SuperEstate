bd4b421b3649b75d1a9e90c940672781245bd09e
Tsai-wei Li <60534676+Whitney1007@users.noreply.github.com>
2020-11-17 18:50:30 +0800

    Adding Readmore function for description

--- getDetails.php
+++ getDetails.php
@@ -20,7 +20,8 @@ echo "<style type='text/css'>";
 echo "body{background-color:#EFEFEF; font-family:Segoe UI; font-weight:bold; padding:8px}";
 echo "h2 {color:#3C7E7E;}";
 echo "h4 {margin:0;}";
-echo "li,p {font-family:Adobe Clean;font-weight: normal;font-size: 18px;}";
+echo "li,p {font-family:Adobe Clean;font-weight: normal;font-size: 16px;}";
+echo "#seelink{text-align:left;padding-left:0px;text-decoration:none;color:red;font-style:italic;}";
 echo "a:link{text-align:center;display:inline-block;padding-left:60px;}";
 echo "a:visited{color:#112CF5;};";
 echo "</style>";
@@ -45,7 +46,7 @@ if (mysqli_num_rows($result)>0){
 			default:
 				$guide = "index.html";
 		}
-
+		
 		echo "<h2>$".number_format($row["price"])."</h2>";
 		echo "<h4>Property Details</h4>";
 		echo "<ul>";
@@ -54,14 +55,42 @@ if (mysqli_num_rows($result)>0){
 		echo "<li>".$row["parking_type"]."</li>";
 		echo "<li>Built in ".$row["build_year"]."</li></ul>";
 		echo "<h4>Overview</h4>";
-		echo "<p>".$row["description"]."</p>";
-		echo "<br><br>";
+		
+		if(strlen($row["description"]) > 350){
+			$textdisplaylist = substr($row["description"],0,350)."<span id='dots'>...</span><span id='more' style='display:none;'>";
+			$textdisplaylist.=substr($row["description"],350)."</span>";
+			$textdisplaylist.="<a href='#' id='seelink' onclick='seemore();'>Read More</a>";
+		}
+		else{
+			$textdisplaylist = $row["description"];
+		}
+		
+		echo "<p>".$textdisplaylist."</p>";
 		echo "<a href='".$guide."' target='_blank'>Neighborhood Information For Zip Code ";
 		echo $zipcode."</a>";
 	}
 }else{
 	echo "<h2>No result</h2>";
 }
+
+echo '<script>
+		function seemore() {
+			var dots = document.getElementById("dots");
+			var moreText = document.getElementById("more");
+			var btnText = document.getElementById("seelink");
+
+			if (dots.style.display === "none") {
+				dots.style.display = "inline";
+				btnText.innerHTML = "Read more"; 
+				moreText.style.display = "none";
+			} else {
+				dots.style.display = "none";
+				btnText.innerHTML = "Read less"; 
+				moreText.style.display = "inline";
+			}
+		}
+	</script>';
+
 
 
 mysqli_close($conn);
