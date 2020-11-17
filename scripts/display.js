e4da925377b371f613647cb8572f300499d9717c
Greg <greg.diehl.gtd@gmail.com>
2020-11-17 11:46:34 -0800

    Merging display.js

--- scripts/display.js
+++ scripts/display.js
@@ -19,3 +19,56 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+function isFavProperty(uname) {
+  var propertyId = getURLParam('id');
+
+  jQuery.ajax({
+    type: "POST",
+    url: 'getFavProperty.php',
+    dataType: 'json',
+    data: {functionname: 'fav', arguments: [uname, propertyId]},
+
+    success: function (obj, textstatus) {
+                  if( !('error' in obj) ) {
+                    setFavProperty(obj.result);
+                  }
+                  else {
+                      console.log(obj.error);
+                  }
+            }
+    });
+}
+
+function clickedFav(uname) {
+  var propertyId = getURLParam('id');
+
+  if(document.getElementById("favStar").innerHTML == "\u2b50") {
+    propertyId = -propertyId;
+  } 
+
+  jQuery.ajax({
+    type: "POST",
+    url: 'setFavProperty.php',
+    dataType: 'json',
+    data: {functionname: 'fav', arguments: [uname, propertyId]},
+
+    success: function (obj, textstatus) {
+                  if( !('error' in obj) ) {
+                    setFavProperty(obj.result);
+                  }
+                  else {
+                      console.log(obj.error);
+                  }
+            }
+    });
+}
+
+function setFavProperty(result) {
+  console.log(result);
+  if(result) {
+    document.getElementById("favStar").innerHTML = "\u2b50";
+  } else {
+    document.getElementById("favStar").innerHTML = "\u2606";
+  }
+}
