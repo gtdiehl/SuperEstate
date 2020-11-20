5a92bc0db914b26db54f8e1e1efa3196fa17d46c
Tsai-wei Li <60534676+Whitney1007@users.noreply.github.com>
2020-11-20 12:28:42 -0800

    Adding check connection

--- config.php
+++ config.php
@@ -4,4 +4,9 @@
    define('DB_PASSWORD', 'super12345');
    define('DB_DATABASE', 'superestate');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   //check connection
+	if (!$db){
+		die("Connection failed: ".mysqli_connect_error());
+	}
 ?>
