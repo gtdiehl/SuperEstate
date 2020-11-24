4dce5b9258a30e66726b9538a40faef506a2b2e8
Greg <greg.diehl.gtd@gmail.com>
2020-11-24 15:54:13 -0800

    Removing code/files not used anymore

--- scripts/utils.js
+++ scripts/utils.js
@@ -8,15 +8,3 @@
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
-
-// TODO: Remove function appears not to be used anymore
-function sortByProperty(property){  
-    return function(a,b){  
-       if(a[property] > b[property])  
-          return 1;  
-       else if(a[property] < b[property])  
-          return -1;  
-   
-       return 0;  
-   }  
-}
