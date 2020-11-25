75c43e971eff2e03bc18230021115e1956223eef
Greg <greg.diehl.gtd@gmail.com>
2020-11-24 19:21:25 -0800

    Better layout for My Property page
    When no properties are saved.

--- getMyProperty.php
+++ getMyProperty.php
@@ -12,9 +12,14 @@
     echo "<link rel='stylesheet' type='text/css' href='styles/styles.css'></link>";
 
     // Display HTML text and logout link
-    // TODO: Need a better layout!
-    echo "<h1>My Saved Properties</h1>";
+    echo "<div class='savedPropertyTop'>";
+    echo "<div class='savedPropertyColumn'>";
+    echo "<h1 id='savedPropertyHeading'>My Saved Properties</h1>";
+    echo "</div>";
+    echo "<div class='savedPropertyColumn' id='floatRight'>";
     echo "<h2><a href = 'logout.php'>Sign Out</a></h2>";
+    echo "</div>";
+    echo "</div>";
 
     $result=mysqli_query($db,$sql);
 
@@ -36,7 +41,8 @@
                 }
             // If the result is null, tell the user they have not saved any properties.
             } else {
-                echo "<h2>No Saved Properties. Click on the favorite Star icon on the Property page to save your favorite property.</h2>";
+                echo "<h3 class='noSavedProperties'>No Saved Properties</h3>";
+                echo "<h3 class='nosavedProperties' id='propMesg'>Click on the ☆ icon in the Property page to save your favorite property.</h3>";
             }
         }
     }
