94b4da161fde52190cfdce3187d0b2404efe3b64
Tsai-wei Li <60534676+Whitney1007@users.noreply.github.com>
2020-11-30 18:31:28 +0800

    Make a better layout for account page

--- myproperty.php
+++ myproperty.php
@@ -11,20 +11,20 @@
     <script src="scripts/navLoaded.js"></script>
     <script src="scripts/utils.js"></script>
     <body onload="navLoaded();footerLoaded();">
-      <div class="content">
+      <div class="accountcontainer">
         <script type="text/javascript">
-          // Retrieve logged in user name from PHP sesssion info
-          var x = "<?php echo $login_session ?>"; 
+		// Retrieve logged in user name from PHP sesssion info
+		var x = "<?php echo $login_session ?>"; 
 
-          // Hidden form to submit username to server and display results in iFrame
-          // getMyProperty.php displays the results
-          document.writeln('<form id="hiddenform" action="getMyProperty.php" target="my_iframe" method="post">');
-          document.writeln("<input type='text' name='username' value='" + x + "'>");
-          document.writeln('<input type="submit" value="post">');
-          document.writeln('</form>');
-          document.getElementById("hiddenform").style.display = "none";
-          document.writeln('<iframe name="my_iframe" id="myPropertyFrame"></iframe>');
-          document.forms["hiddenform"].submit();
+		// Hidden form to submit username to server and display results in iFrame
+		// getMyProperty.php displays the results
+		document.writeln('<form id="hiddenform" action="getMyProperty.php" target="my_iframe" method="post">');
+		document.writeln("<input type='text' name='username' value='" + x + "'>");
+		document.writeln('<input type="submit" value="post">');
+		document.writeln('</form>');
+		document.getElementById("hiddenform").style.display = "none";
+		document.writeln('<iframe name="my_iframe" id="myPropertyFrame"></iframe>');
+		document.forms["hiddenform"].submit();
         </script>
       </div>
     </body>
